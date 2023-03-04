bab5cc21758b1d329a33c1c4be0664e6e5b213bc
Dmytro Karpenko <kardmitriy@gmail.com>
2023-03-04 15:24:10 +0200

    debouce

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import fetchCountries from './js/fetchCountries';
 import { listItemTemplate } from './templates/listItemTemplate';
 import { countryCardTemplate } from './templates/countryCardTemplate';
 
-import { debounce } from 'lodash';
+import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
